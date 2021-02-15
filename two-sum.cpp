c20cc029260f44c50612f38e7df3b69a002db4c9
isanchez-aguilar <saais31@gmail.com>
2021-02-15 01:20:17 -0600

    Comment about complexity added for problem Two Sum.

--- two-sum.cpp
+++ two-sum.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-      vector<int> solution(2);
-      const int size = (int)nums.size();
-      unordered_map<int, int> previousValues;
+  /*
+  ** Complexity time: O(n)
+  ** Complexity space: O(n)
+  */
+  vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> solution(2);
+    
+    const int size = (int)nums.size();
+    unordered_map<int, int> previousValues;
+    
+    for (int i = 0; i < size; ++i) {
+      const int remaining = target - nums[i];
       
-      for (int i = 0; i < size; ++i) {
-        const int remaining = target - nums[i];
-        
-        if (previousValues.count(remaining) > 0) {
-          solution[0] = i;
-          solution[1] = previousValues[remaining];
-          break;
-        }
-        
-        previousValues[nums[i]] = i;
-      } 
+      if (previousValues.count(remaining) > 0) {
+        solution[0] = i;
+        solution[1] = previousValues[remaining];
+        break;
+      }
       
-      return solution;
-    }
+      previousValues[nums[i]] = i;
+    } 
+    
+    return solution;
+  }
 };
