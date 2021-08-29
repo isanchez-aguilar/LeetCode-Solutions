1e5cad267a0b539ba266902328d90b3d976dfc33
Isaac Sanchez Aguilar <saais31@gmail.com>
2021-08-29 18:07:02 -0500

    Better solution.

--- reverse-integer.cpp
+++ reverse-integer.cpp
@@ -1,78 +1,37 @@
-string toStringNumber(int x, bool reverseString = false) {
-	bool firstNonZero = false;
-	string number = "";
-		
-	while (x != 0) {
-		int withoutLastDigit = x / 10 * 10; 
-		short lastDigit = (short)(x - withoutLastDigit);
-
-		if (lastDigit < 0)
-			lastDigit = -lastDigit;
-		
-		if (firstNonZero or lastDigit != 0) {
-			firstNonZero = true;
-			number += char(lastDigit + '0');
-    }
-
-		x /= 10;
-	}
-  
-  if (reverseString)
-    reverse(number.begin(), number.end());
-
-	return number;
-}
-
-string minNumber = toStringNumber(INT_MIN, true);
-string maxNumber = toStringNumber(INT_MAX, true);
-
-
 class Solution {
 public:
-	/*
-	** Time complexity: O(log2(x))
-	** Space complexity: O(log2(x))
-	*/
-  int reverse(int x) {
-		if (x == 0)
-			return 0;
-
-		const string number = toStringNumber(x);
-
-		if (x > 0 and isGreater(number, maxNumber)) 
-			return 0; 
-
-		if (x < 0 and isGreater(number, minNumber))
-			return 0;
-		
-		int reversedNumber = toInteger(number);
-
-		if (x < 0)
-			return -reversedNumber;
-
-		return reversedNumber;
-	}
-
-	static bool isGreater(const string& a, const string& b) {
-		if (a.size() < b.size())
-			return false;
-
-		if (a.size() > b.size())
-			return true;
+  static const int lastDigitMax = INT_MAX % 10;
+  static const int lastDigitMin = INT_MIN % 10;
     
-		return a > b;
-	}
-
-	static int toInteger(const string& number) {
-		int solution = 0;
-
-		for (const char& digit : number) {
-			solution *= 10;
-
-			solution += (int)digit - '0';
-		}
-
-		return solution;
-	}
+  static bool isOverflowSum(int a, int b) {
+    // Positive integer
+    if (a > INT_MAX / 10 or (a == INT_MAX / 10 and b > lastDigitMax))
+      return true;
+    
+    // Negative integer
+    if (a < INT_MIN / 10 or (a == INT_MIN / 10 and b < lastDigitMin))
+      return true;
+    
+    return false;
+  }
+ 
+  /*
+  ** Time complexity: O(log10 x)
+  ** Space complexity: O(1)
+  */
+  int reverse(int x) {
+    int result = 0;
+    
+    while (x != 0) {
+      const int lastDigit = x % 10;
+      x /= 10;
+      
+      if (isOverflowSum(result, lastDigit))
+        return 0;
+      
+      result = 10 * result + lastDigit;
+    }
+    
+    return result;
+  }
 };
-
